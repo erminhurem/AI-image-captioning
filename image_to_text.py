f86711d7f736a533e98131dfd6a037bd41f4c341
Ermin Huremović <102902532+erminhurem@users.noreply.github.com>
2024-03-23 17:53:27 +0100

    Update image_to_text.py

--- image_to_text.py
+++ image_to_text.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer xxxxxxxxxxxxxxxxxxxxxxxxx"}
 
 def generate_semantics(file):
     response = requests.post(API_URL, headers=headers, data=file)
