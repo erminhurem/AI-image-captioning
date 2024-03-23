b3c625d0a5b17feddb7b1c45cd3a05aa0f703797
erminhurem <ermin.huremovic90@gmail.com>
2024-03-23 16:11:04 +0100

    name change

---
+++ image_camption.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
+
+print(output[0]["generated_text"])
